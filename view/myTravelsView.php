24cdce5f944a03cd042db3e91bf8d166c8a85477
JonathanMartins <jonathan_l_m@hotmail.com>
2020-11-10 15:28:57 -0300

    myTravelsView agregada

---
+++ view/myTravelsView.php
@@ -0,0 +1,203 @@
+{{> header}}
+
+<main>
+
+    <div class="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-right" id="mySidebar">
+        <button class="w3-bar-item w3-button w3-large w3-hide-large" onclick="w3_close()">Cerrar &times;</button>
+        <h3 class="w3-bar-item">Menú</h3>
+        <a href="#" class="w3-bar-item w3-button">Mis Viajes</a>
+        <a href="#" class="w3-bar-item w3-button">Nuevo viaje</a>
+        <a href="#" class="w3-bar-item w3-button">Facturación</a>
+    </div>
+
+    <div class="w3-content">
+        <div class="w3-margin">
+            <input type="text" class="w3-bar-item w3-input w3-border w3-round w3-margin-bottom" style="margin-top: 2.5em;" placeholder="Buscar Viaje..">
+
+            <button class="w3-button w3-green w3-padding-large w3-round w3-right" style="margin-bottom: 2.5em;">Nuevo Viaje</button>
+
+            <div>
+                <div class="w3-row-padding">
+                    <div class="w3-col s12 m6 l4 w3-center" style="margin-bottom: 2em;">
+                        <div class="w3-card-4 w3-round">
+                            <header class="w3-container w3-light-grey d-flex-centrado">
+                                <img src="../public/images/qr.png" alt="user" class="w3-left w3-image w3-margin-top" style="width:50%;">
+                            </header>
+
+                            <div class="w3-container w3-margin-bottom">
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <div>
+                                        <h3>Desde:</h3>
+                                        <p>Buenos Aires</p>
+                                    </div>
+                                    <div>
+                                        <h3>Hasta:</h3>
+                                        <p>Formosa</p>
+                                    </div>
+                                </div>
+
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <h3 class="margin-0">Cliente:</h3>
+                                    <p class="margin-0">CompuGrande</p>
+                                </div>
+
+                                <button class="w3-button w3-red w3-tiny w3-round w3-margin-bottom">Eliminar</button>
+                                <button class="w3-button w3-blue w3-tiny w3-round w3-margin-bottom">Editar</button>
+                                <button class="w3-button w3-green w3-tiny w3-round w3-margin-bottom">Ver Viaje</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w3-col s12 m6 l4 w3-center" style="margin-bottom: 2em;">
+                        <div class="w3-card-4 w3-round">
+                            <header class="w3-container w3-light-grey d-flex-centrado">
+                                <img src="../public/images/qr.png" alt="user" class="w3-left w3-image w3-margin-top" style="width:50%;">
+                            </header>
+
+                            <div class="w3-container w3-margin-bottom">
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <div>
+                                        <h3>Desde:</h3>
+                                        <p>Chubut</p>
+                                    </div>
+                                    <div>
+                                        <h3>Hasta:</h3>
+                                        <p>Jujuy</p>
+                                    </div>
+                                </div>
+
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <h3 class="margin-0">Cliente:</h3>
+                                    <p class="margin-0">LocoBrothers</p>
+                                </div>
+
+                                <button class="w3-button w3-red w3-tiny w3-round w3-margin-bottom">Eliminar</button>
+                                <button class="w3-button w3-blue w3-tiny w3-round w3-margin-bottom">Editar</button>
+                                <button class="w3-button w3-green w3-tiny w3-round w3-margin-bottom">Ver Viaje</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w3-col s12 m6 l4 w3-center" style="margin-bottom: 2em;">
+                        <div class="w3-card-4 w3-round">
+                            <header class="w3-container w3-light-grey d-flex-centrado">
+                                <img src="../public/images/qr.png" alt="user" class="w3-left w3-image w3-margin-top" style="width:50%;">
+                            </header>
+
+                            <div class="w3-container w3-margin-bottom">
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <div>
+                                        <h3>Desde:</h3>
+                                        <p>Entre Rios</p>
+                                    </div>
+                                    <div>
+                                        <h3>Hasta:</h3>
+                                        <p>Santa Fe</p>
+                                    </div>
+                                </div>
+
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <h3 class="margin-0">Cliente:</h3>
+                                    <p class="margin-0">Pirelli</p>
+                                </div>
+
+                                <button class="w3-button w3-red w3-tiny w3-round w3-margin-bottom">Eliminar</button>
+                                <button class="w3-button w3-blue w3-tiny w3-round w3-margin-bottom">Editar</button>
+                                <button class="w3-button w3-green w3-tiny w3-round w3-margin-bottom">Ver Viaje</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w3-col s12 m6 l4 w3-center" style="margin-bottom: 2em;">
+                        <div class="w3-card-4 w3-round">
+                            <header class="w3-container w3-light-grey d-flex-centrado">
+                                <img src="../public/images/qr.png" alt="user" class="w3-left w3-image w3-margin-top" style="width:50%;">
+                            </header>
+
+                            <div class="w3-container w3-margin-bottom">
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <div>
+                                        <h3>Desde:</h3>
+                                        <p>Buenos Aires</p>
+                                    </div>
+                                    <div>
+                                        <h3>Hasta:</h3>
+                                        <p>Mendoza</p>
+                                    </div>
+                                </div>
+
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <h3 class="margin-0">Cliente:</h3>
+                                    <p class="margin-0">Musimundo</p>
+                                </div>
+
+                                <button class="w3-button w3-red w3-tiny w3-round w3-margin-bottom">Eliminar</button>
+                                <button class="w3-button w3-blue w3-tiny w3-round w3-margin-bottom">Editar</button>
+                                <button class="w3-button w3-green w3-tiny w3-round w3-margin-bottom">Ver Viaje</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w3-col s12 m6 l4 w3-center" style="margin-bottom: 2em;">
+                        <div class="w3-card-4 w3-round">
+                            <header class="w3-container w3-light-grey d-flex-centrado">
+                                <img src="../public/images/qr.png" alt="user" class="w3-left w3-image w3-margin-top" style="width:50%;">
+                            </header>
+
+                            <div class="w3-container w3-margin-bottom">
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <div>
+                                        <h3>Desde:</h3>
+                                        <p>Santa Fe</p>
+                                    </div>
+                                    <div>
+                                        <h3>Hasta:</h3>
+                                        <p>Rio Negro</p>
+                                    </div>
+                                </div>
+
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <h3 class="margin-0">Cliente:</h3>
+                                    <p class="margin-0">Toro Rojo</p>
+                                </div>
+
+                                <button class="w3-button w3-red w3-tiny w3-round w3-margin-bottom">Eliminar</button>
+                                <button class="w3-button w3-blue w3-tiny w3-round w3-margin-bottom">Editar</button>
+                                <button class="w3-button w3-green w3-tiny w3-round w3-margin-bottom">Ver Viaje</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w3-col s12 m6 l4 w3-center" style="margin-bottom: 2em;">
+                        <div class="w3-card-4 w3-round">
+                            <header class="w3-container w3-light-grey d-flex-centrado">
+                                <img src="../public/images/qr.png" alt="user" class="w3-left w3-image w3-margin-top" style="width:50%;">
+                            </header>
+
+                            <div class="w3-container w3-margin-bottom">
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <div>
+                                        <h3>Desde:</h3>
+                                        <p>Catamarca</p>
+                                    </div>
+                                    <div>
+                                        <h3>Hasta:</h3>
+                                        <p>Tucuman</p>
+                                    </div>
+                                </div>
+
+                                <div class="detalles-viaje w3-margin-bottom">
+                                    <h3 class="margin-0">Cliente:</h3>
+                                    <p class="margin-0">Sin Limites S.A</p>
+                                </div>
+
+                                <button class="w3-button w3-red w3-tiny w3-round w3-margin-bottom">Eliminar</button>
+                                <button class="w3-button w3-blue w3-tiny w3-round w3-margin-bottom">Editar</button>
+                                <button class="w3-button w3-green w3-tiny w3-round w3-margin-bottom">Ver Viaje</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</main>
+
+{{> footer}}
