68d8ae8c6107d1fb0bfe44840e4d48b290c57fca
Gaston Perez <gastonperez-97@hotmail.com>
2020-11-17 18:39:40 -0300

    added usuarios button on homeView

--- view/homeView.php
+++ view/homeView.php
@@ -21,6 +21,11 @@
             <a href="service" class="w3-button w3-block w3-amber w3-padding-large w3-round-large" ><b>Mantenimiento de Vehiculos<b></a>
         </div>
     </div>
+    <div class="w3-content w3-section">
+        <div class="w3-container ">
+            <a href="/pw2-grupo03/usuarios" class="w3-button w3-block w3-amber w3-padding-large w3-round-large" ><b>Usuarios<b></a>
+        </div>
+    </div>
 </div>
 
 {{> footer}}
