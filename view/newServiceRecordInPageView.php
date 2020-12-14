f6ee7d5fc786e131d64340f824788605d109fd2b
ldinubila <lucas.dinubila@gmail.com>
2020-12-14 19:39:44 -0300

    se carga la data-table y se da la posibilidar de generar el PDF

--- view/newServiceRecordInPageView.php
+++ view/newServiceRecordInPageView.php
@@ -1,1 +1,36 @@
-<?php
+{{> headerWithSidebar}}
+
+<div class="container-80pct">
+    <div class="w3-margin">
+        <a href="/pw2-grupo03/report/newServiceRecord" class="w3-button w3-green w3-padding-large w3-round w3-right" style="margin-bottom: 2.5em;">Exportar PDF</a>
+
+        <div class="my-2em">
+            <table id="data-table" class="w3-table w3-border w3-bordered w3-centered display responsive nowrap" style="width:100%">
+                <thead>
+                <tr class="table-head">
+                    <th class="vertical-align-middle">N° Service</th>
+                    <th class="vertical-align-middle">N° Unidad</th>
+                    <th class="vertical-align-middle">Fecha</th>
+                    <th class="vertical-align-middle">Kilometros</th>
+                    <th class="vertical-align-middle">Descripción</th>
+                    <th class="vertical-align-middle">Costo</th>
+                </tr>
+                </thead>
+                <tbody>
+                {{#servicesByDate}}
+                <tr>
+                    <td class="vertical-align-middle">{{id_service}}</td>
+                    <td class="vertical-align-middle">{{vehiculo}}</td>
+                    <td class="vertical-align-middle">{{fecha_service}}</td>
+                    <td class="vertical-align-middle">{{KM}}</td>
+                    <td class="vertical-align-middle">{{detalle}}</td>
+                    <td class="vertical-align-middle">{{costo}}</td>
+                </tr>
+                {{/servicesByDate}}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+{{> footerSidebarFixed}}
